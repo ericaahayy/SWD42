92596c2ac43ca788a9a01ca43fd0ba1bcdf39021
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-03-18 01:37:14 -0500

    Changes

--- server/db.js
+++ server/db.js
@@ -1,14 +1,12 @@
 const mysql = require("mysql");
 let instance = null;
 
-// Change this connection to connect to your own local server
-// We will change this to a cloud server later.
-const connection = mysql.createConnection({
-    host: "localhost",
-    database: "swd42group",
-    user: "root",
-    password: "Localhost123!",
-});
+// const connection = mysql.createConnection({
+//     host: "127.0.0.1",
+//     database: "localhost",
+//     user: "root",
+//     password: "root123",
+//   });
 
 connection.connect((err) => {
     if (err) throw err;
--- server/server.js
+++ server/server.js
@@ -2,3 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 500;
+
+app.use(cors());
+app.use(express());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
