8e4ec96123e76c7ec6e63e384f9a961ff5bcdc68
erica <ericakimhay03@gmail.com>
2024-03-27 15:24:54 -0500

    passed another

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 500;
+const port = 5001;
 const dbService = require("./db");
 
 app.use(cors());
